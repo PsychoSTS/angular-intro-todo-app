f24b20f192164f097deb01ead8f275f5463cb266
Roelof <roelof@labs.epiuse.com>
2018-05-06 20:48:32 +0200

    Added cors. Added todos to the /todos route

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const {todos} = require('./todos');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/todos', (req, res) => {
     res.send({
-        todos: [
-            {
-                title: 'Title',
-                description: 'description'
-            }
-        ]
+        todos
     });
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('Please use the /todos route');
 });
 
 app.listen(PORT, () => {
---
+++ todos.js
@@ -0,0 +1,18 @@
+let todos = [
+    {
+        title: 'Walk the dog',
+        description: 'Walk Olga the Belgium shepert around the block at 6:00pm.',
+        edit: false,
+        done: false
+    },
+    {
+        title: 'Buy groceries',
+        description: 'Buy groceries for the next week.',
+        edit: false,
+        done: false
+    }
+];
+
+module.exports = {
+    todos
+}
